7cddaffcc8b8e90d735147b4c3fa26fe40b78d98
MelatDB <melatdesta101@gmail.com>
2025-07-23 15:54:32 +0100

    hellocontroller changed

--- app/src/main/java/com/example/DoctorPlaza/Backend/HelloController.java
+++ app/src/main/java/com/example/DoctorPlaza/Backend/HelloController.java
@@ -16,6 +16,6 @@ public class HelloController {
     
     @GetMapping("/")
     public String index(){
-        return "Hello from here";
+        return "Hello from here with some sugar";
     }
 }
