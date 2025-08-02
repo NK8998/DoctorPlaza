c9150dceacf0e1d1b481d83f712490dafef0d134
Chribela <christabellanibaruta@gmail.com>
2025-08-02 19:00:39 +0100

    fixed patient history

--- app/src/main/java/com/example/DoctorPlaza/Frontend/controllers/Patient_historyController.java
+++ app/src/main/java/com/example/DoctorPlaza/Frontend/controllers/Patient_historyController.java
@@ -4,6 +4,7 @@
  */
 package com.example.DoctorPlaza.Frontend.controllers;
 
+import com.example.DoctorPlaza.Frontend.SceneManager;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -30,25 +31,36 @@ public class Patient_historyController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+   @FXML
 
-    @FXML
-    private void handleDashboard(ActionEvent event) {
+       private void handleDashboard(ActionEvent event) {
+        SceneManager.switchScene("com/example/DoctorPlaza/Frontend/doctor/dashboard.fxml", new DashboardController());
     }
 
     @FXML
     private void handlePatientQueue(ActionEvent event) {
+        
+       SceneManager.switchScene("com/example/DoctorPlaza/Frontend/doctor/patient_queue.fxml", new Patient_queueController());
+    }
+
+    @FXML
+    private void handlePatientHistory(ActionEvent event) {
+        System.out.println("Already on Patient History page. ");
     }
 
     @FXML
     private void handleAddRecord(ActionEvent event) {
+        
+        SceneManager.switchScene("com/example/DoctorPlaza/Frontend/doctor/add_record.fxml", new Add_recordController());
     }
 
     @FXML
-    private void handleEditRecords(ActionEvent event) {
+    private void handleEditRecord(ActionEvent event) {
+        SceneManager.switchScene("com/example/DoctorPlaza/Frontend/doctor/edit_record.fxml", new Edit_recordController());
     }
-    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
